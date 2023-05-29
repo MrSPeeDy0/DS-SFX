a013ae870aff3c025fed91167b68723d69bd55af
Ultra <132045787+MrSPeeDy0@users.noreply.github.com>
2023-05-29 21:24:29 +0800

    First SFX.lua

--- First SFX.lua
+++ First SFX.lua
@@ -2,5 +2,7 @@
     sound.SoundId = "rbxassetid://5602666105"
     sound.Volume = 100000
     sound:Play()
+
 task.wait(1.0)
+
 loadstring(game:HttpGet("https://raw.githubusercontent.com/MrSPeeDy0/DS-SFX/main/First%20SFX.lua"))()
